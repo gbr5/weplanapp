b169f9d7373e5a898fcf8ff1d467cf5b527f451e
GuyBR <gbrobbe@gmail.com>
2021-05-09 23:22:28 -0300

    S_291:App-Disable signingConfigs from machine

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
+import * as Sentry from '@sentry/react-native';
 import AppProvider from './hooks';
 import Routes from './routes';
 
 import { theme } from './global';
 
+Sentry.init({
+  dsn: 'https://4472efcf6b6042f2bd925a1a2d48f434@o637257.ingest.sentry.io/5756133',
+});
+// throw new Error('My first Sentry error!');
 const App: React.FC = () => {
   useEffect(() => {
     SplashScreen.hide();
